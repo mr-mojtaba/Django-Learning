bf52c2963c02ee7fcd294033b786e5d27295800d
mr-mojtaba <mr.mojtaba.mailgmail.com>
2024-01-01 20:56:31 +0330

    add * for runserver ports

--- SabzWeb/SabzWeb/settings.py
+++ SabzWeb/SabzWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%0(^biuwig-om3p0jx4i-@jstnu*kcp#3cy65oyahw(m#^vso#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
