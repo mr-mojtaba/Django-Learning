325f0152b6fa96ac9e1cbdfcccae5065defb53d2
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-01-22 18:51:26 +0330

    Creating pagination and managing exceptions.

--- SabzWeb/blog/views.py
+++ SabzWeb/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -10,10 +11,23 @@ def index(request):
 
 def post_list(request):
     posts = Post.published.all()
+
+    # Creating an object of the paginator class.
+    paginator = Paginator(posts, 2)
+    # Specifying the page number.
+    page_number = request.GET.get('page', 1)
+    # Resetting the value with page_number.
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+
     context = {
         'posts': posts,
     }
-    return render(request, "blog/list.html", context)
+    return render(request,"blog/list.html", context)
 
 
 def post_detail(request, id):
