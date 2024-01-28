ad44e996da0fe6ba235695c04ab20048185a6216
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-01-28 19:30:13 +0330

    Removing subject choices tuple.

--- SabzWeb/blog/forms.py
+++ SabzWeb/blog/forms.py
@@ -2,13 +2,9 @@ from django import forms
 
 
 class TicketForm(forms.Form):
-    SUBJECT_CHOICES = (
-        ('پیشنهاد', 'پیشنهاد'),
-        ('انتقاد', 'انتقاد'),
-        ('گزارش', 'گزارش'),
-    )
+
     message = forms.CharField(widget=forms.Textarea, required=True)
     name = forms.CharField(max_length=250, required=True)
     email = forms.EmailField()
     phone = forms.CharField(max_length=11, required=True)
-    subject = forms.ChoiceField(choices=SUBJECT_CHOICES)
+    subject = forms.CharField()
