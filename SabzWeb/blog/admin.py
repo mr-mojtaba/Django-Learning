bf9f251bc9ed06c21474ed38ccd9b7c470b9bb9c
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-01-29 17:46:10 +0330

    Adding the CommentAdmin class to admin.py

--- SabzWeb/blog/admin.py
+++ SabzWeb/blog/admin.py
@@ -18,34 +18,92 @@ admin.sites.AdminSite.index_title = "پنل مدیریت"
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     # Showing these fields in the top bar of the posts display.
-    list_display = ['title', 'author', 'publish', 'status']
+    list_display = [
+        'title',
+        'author',
+        'publish',
+        'status',
+    ]
 
     # Defining the default list for sorting fields.
-    ordering = ['title', 'author']
+    ordering = [
+        'title',
+        'author',
+    ]
 
     # Creating a search bar and a list of searchable fields.
-    search_fields = ['title', 'description']
+    search_fields = [
+        'title',
+        'description',
+    ]
 
     # Creating a filter page and a list of filterable fields.
-    list_filter = ('status', ('publish', JDateFieldListFilter), 'author')
+    list_filter = (
+        'status',
+        ('publish', JDateFieldListFilter),
+        'author',
+    )
 
     # Assigning an ID to each author and
     # changing the type of author editing in the post manager.
-    raw_id_fields = ['author']
+    raw_id_fields = [
+        'author',
+    ]
 
     # Creating a time hierarchy filter based on a field.
     date_hierarchy = 'publish'
 
     # To automatically complete the slug field based on title.
-    prepopulated_fields = {'slug': ['title']}
+    prepopulated_fields = {
+        'slug': ['title'],
+    }
 
     # Creating the ability to edit each field in the table.
-    list_editable = ['status']
+    list_editable = [
+        'status',
+    ]
 
     # Converting each field to a link for opening the post manager page.
-    list_display_links = ['title', 'author', 'publish']
+    list_display_links = [
+        'title',
+        'author',
+        'publish',
+    ]
 
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
-    list_display = ['name', 'subject', 'phone']
+    list_display = [
+        'name',
+        'subject',
+        'phone',
+    ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    # Showing these fields in the top bar of the posts display.
+    list_display = [
+        'post',
+        'name',
+        'created',
+        'active',
+    ]
+
+    # Creating a filter page and a list of filterable fields.
+    list_filter = (
+        'active',
+        ('created', JDateFieldListFilter),
+        ('updated', JDateFieldListFilter),
+    )
+
+    # Creating a search bar and a list of searchable fields.
+    search_fields = [
+        'name',
+        'bode',
+    ]
+
+    # Creating the ability to edit each field in the table.
+    list_editable = [
+        'active',
+    ]
