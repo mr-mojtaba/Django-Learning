65b33748df83238dfc01842177024208396a523d
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-01-30 16:49:09 +0330

    Adding the comment url

--- SabzWeb/blog/urls.py
+++ SabzWeb/blog/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
 
     # Path definition for functional views
     # path('posts/', views.post_list, name='post_list'),
-    # path('posts/<int:id>', views.post_detail, name='post_detail'),
+    path('posts/<int:id>', views.post_detail, name='post_detail'),
+    path('posts/<int:id>/comment', views.post_comment, name='post_comment'),
 
     # Path definition for class-based views
     path('posts/', views.PostListView.as_view(), name='post_list'),
-    path('posts/<pk>', views.PostDetailView.as_view(), name='post_detail'),
+    # path('posts/<pk>', views.PostDetailView.as_view(), name='post_detail'),
     path('ticket', views.ticket, name='ticket'),
 ]
