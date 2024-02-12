027db7b32bb4d1ffc1a569583f08f57e190ce9b8
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-02-12 21:19:19 +0330

    Adding the create_post url

--- SabzWeb/blog/urls.py
+++ SabzWeb/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import create_post
 
 app_name = 'blog'
 
@@ -10,9 +11,10 @@ urlpatterns = [
     # path('posts/', views.post_list, name='post_list'),
     path('posts/<int:id>', views.post_detail, name='post_detail'),
     path('posts/<int:post_id>/comment', views.post_comment, name='post_comment'),
+    path('ticket', views.ticket, name='ticket'),
+    path('create_post/', create_post, name='create_post'),
 
     # Path definition for class-based views
     path('posts/', views.PostListView.as_view(), name='post_list'),
     # path('posts/<pk>', views.PostDetailView.as_view(), name='post_detail'),
-    path('ticket', views.ticket, name='ticket'),
 ]
