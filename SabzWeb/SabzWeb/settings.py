8dbcc4467fa3363d1bd466b310eb1874b1239bb4
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-07-28 12:43:30 +0330

    Change venv

--- SabzWeb/SabzWeb/settings.py
+++ SabzWeb/SabzWeb/settings.py
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'SabzWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'blog_admin',
-        'PASSWORD': '13745123',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'blog',
+#         'USER': 'blog_admin',
+#         'PASSWORD': '13745123',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
