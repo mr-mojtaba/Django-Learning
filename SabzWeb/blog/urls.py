bc3d05bd9f0e7028428995aecd6ca53b084fee10
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-11-06 12:51:40 +0330

    Make site template changes

--- SabzWeb/blog/urls.py
+++ SabzWeb/blog/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     # path('posts/<pk>', views.PostDetailView.as_view(), name='post_detail'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_forme.html' ,success_url='done'), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
